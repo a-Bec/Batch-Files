a72daf00023f84f3c34fd3bdadce754147be0c35
a-Bec <97602229+a-Bec@users.noreply.github.com>
2023-08-14 20:23:25 +0200

    Add files via upload

---
+++ StabilityPing.bat
@@ -0,0 +1,42 @@
+::BATCH FOR TESTING CONNECTION STABILITY
+
+@echo off
+setlocal
+
+REM get Start time
+set "Starttime=%time%"
+echo Start: %Starttime%
+
+REM Options
+set TargetHost=88.99.192.186
+
+REM Number of sended pakets
+set Pakets=1000
+set results=ResultsOfPing.txt
+
+REM Ping running
+ping %TargetHost% -n %Pakets% > %results%
+
+REM show results
+type %results%
+
+echo Ping test end
+
+REM get end time
+set "Endtime=%time%"
+
+REM calculation of seconds
+for /f "tokens=1-3 delims=:." %%a in ("%Starttime%") do (
+    set /a "StartSeconds=(((%%a*60)+1%%b %% 100)*60)+1%%c %% 100"
+)
+for /f "tokens=1-3 delims=:." %%a in ("%Endtime%") do (
+    set /a "EndSeconds=(((%%a*60)+1%%b %% 100)*60)+1%%c %% 100"
+)
+
+set /a "totalDuration=EndSeconds - StartSeconds"
+
+REM show total duration time 
+echo total duration: %totalDuration% seconds
+
+endlocal
+exit /b
